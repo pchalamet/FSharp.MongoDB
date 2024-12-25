44078dec9bd7e5990e981045af4bcca59263046b
Pierre Chalamet <pierre@chalamet.net>
2024-12-25 17:55:15 +0100

    simplify list

--- FSharp.MongoDB.Driver/Serializers/List.fs
+++ FSharp.MongoDB.Driver/Serializers/List.fs
@@ -8,8 +8,7 @@ type internal ListSerializer<'T>() =
     let contentSerializer = BsonSerializer.LookupSerializer(typeof<System.Collections.Generic.IEnumerable<'T>>)
 
     override _.Serialize(context, _, value) =
-        let list = value
-        contentSerializer.Serialize(context, list)
+        contentSerializer.Serialize(context, value)
 
     override _.Deserialize(context, args) =
         let list = contentSerializer.Deserialize(context, args) :?> System.Collections.Generic.IEnumerable<'T>
